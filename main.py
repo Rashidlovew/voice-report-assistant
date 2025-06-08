64df7c7a00aafba67da0d5c070bc4b2cc0d28a47
Rashidlovew <r-r-r-1994@hotmail.com>
2025-06-08 12:45:57 +0400

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 eleven = ElevenLabs(api_key=ELEVENLABS_API_KEY)
-VOICE_ID = "VwC51uc4PUblWEJSPzeo"  # Arabic female voice
+VOICE_ID = "mRdG9GYEjJmIzqbYTidv"  # Arabic female voice
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 CORS(app)
